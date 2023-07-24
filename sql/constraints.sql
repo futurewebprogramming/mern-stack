9321f06c78b45b51d1b2ff4bd726f971b925cb50
futurewebprogramming <123332352+futurewebprogramming@users.noreply.github.com>
2023-07-24 11:13:29 +0500

    Commits added to contraints

--- sql/constraints.sql
+++ sql/constraints.sql
@@ -1,11 +1,16 @@
+-- to show databases
 show databases;
 
+-- to create database
 create database marektstore
 
+-- to check databases;
 show databases;
 
+-- to use or select database
 use marektstore
 
+-- to create table
 create table employees (
     emp_id int primary key,
     emp_name varchar(50),
@@ -15,10 +20,13 @@ create table employees (
 );
 
 
+-- check how many tables are there
 show tables
 
+-- to describe table strcutre data
 describe employees
 
+-- forigen key contraints
 create table orders(
     order_id int primary key,
     emp_id int,
@@ -26,8 +34,10 @@ create table orders(
     foreign key (emp_id) references employees(emp_id)
 )
 
+-- to check table structure
 describe orders
 
+-- create unique and not null and the conditional statement
 create table product(
     product_id int primary key,
     product_name varchar(50) not null,
@@ -35,4 +45,5 @@ create table product(
     product_price int check (product_price >= 10)
 )
 
+-- check table structure
 describe product
